5b66cd31b18c401ec67e5e3bb5a7cd26f87a8153
Porras266 <166577464+Porras266@users.noreply.github.com>
2024-10-20 23:36:55 -0600

    Add files via upload

---
+++ Guia_Didactica_9/Guia_Didactica_9/Calcular_Area_Triangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaEjercicio1
+{
+    internal class Calcular_Area_Triangulo
+    {
+
+        //metodo para calcular el area
+
+        public static double calcular_area_triangulo(double Base, double Altura)
+        {
+            double Area;
+            Area = Base * Altura / 2;
+            return Area;
+        }
+
+        //Metodo para imprimir el resultado
+        public static void ImprimirResultado(double Area)
+        {
+            Console.WriteLine($"El Area del triangulo es: {Area:F2}");
+        }
+
+    }
+}
---
+++ Guia_Didactica_9/Guia_Didactica_9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaEjercicio1
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("El siguiente programa Solicita la base su altura de un triangulo y Calculará el area\n");
+
+Console.WriteLine("Ingrese la altura del triangulo");
+double Altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la base del triangulo");
+double Base = double.Parse(Console.ReadLine());
+
+double Area = Calcular_Area_Triangulo.calcular_area_triangulo(Altura, Base);
+Calcular_Area_Triangulo.ImprimirResultado(Area);
+
+Console.ReadKey();
+
+}
+}
+}
